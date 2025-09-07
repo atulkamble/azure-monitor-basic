b3827d1bc4d2b63d3b3ece1d772ee8ed23e8f290
Atul Kamble <atul_kamble@hotmail.com>
2025-09-07 10:40:59 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -9,5 +9,13 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.6.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.5.0"
+    }
   }
 }

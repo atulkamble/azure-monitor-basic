e119dda67a96cba4afe31e73b67a1be3fad2a3f1
Atul Kamble <atul_kamble@hotmail.com>
2025-09-07 10:35:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,24 @@
-variable "prefix"        { type = string  default = "amdemo" }
-variable "location"      { type = string  default = "eastus" }
-variable "admin_username"{ type = string  default = "azureuser" }
-variable "ssh_public_key"{ type = string  description = "Your SSH public key" }
-variable "alert_email"   { type = string  description = "Email for Action Group alerts" }
+variable "prefix" {
+  type    = string
+  default = "amdemo"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "ssh_public_key" {
+  type        = string
+  description = "Your SSH public key"
+}
+
+variable "alert_email" {
+  type        = string
+  description = "Email for Action Group alerts"
+}

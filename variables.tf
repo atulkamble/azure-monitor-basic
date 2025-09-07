66fff47578c9886a091c1030764eadfcbf3fcc90
Atul Kamble <atul_kamble@hotmail.com>
2025-09-07 10:56:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,17 @@
-variable "prefix"         { type = string  default = "amdemo" }
-variable "location"       { type = string  default = "eastus" }
-variable "admin_username" { type = string  default = "azureuser" }
+variable "prefix" {
+  type    = string
+  default = "amdemo"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
 
 # Email where alerts will go
 variable "alert_email" {
@@ -10,10 +21,16 @@ variable "alert_email" {
 
 # --- Keypair options ---
 # If true, Terraform generates a keypair and uses it automatically.
-variable "generate_key_pair" { type = bool   default = true }
+variable "generate_key_pair" {
+  type    = bool
+  default = true
+}
 
 # File name for generated keys under ./keys/
-variable "key_name"          { type = string default = "amdemo-key" }
+variable "key_name" {
+  type    = string
+  default = "amdemo-key"
+}
 
 # Optional BYO public key (OpenSSH). Used only when generate_key_pair = false.
 variable "ssh_public_key" {
